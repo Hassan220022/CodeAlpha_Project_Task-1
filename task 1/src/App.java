a3cfa809c1049dd1f5ea1eea0485d884f1b7b0da
Hassan220022 <hassansonson2002@gmail.com>
2024-02-15 09:50:40 +0200

    k

--- task 1/src/App.java
+++ task 1/src/App.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
