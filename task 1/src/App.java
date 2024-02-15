33da7d66a4e996cf4f1f417093448667efd82d36
Hassan220022 <hassansonson2002@gmail.com>
2024-02-15 10:02:18 +0200

    task 2 done

--- task 1/src/App.java
+++ task 1/src/App.java
@@ -5,29 +5,30 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> grades = new ArrayList<>();
-        while (true){
+        while (true) {
             System.out.println("Enter a grade (or -1 to finish):");
             int grade = sc.nextInt();
-            if (grade == -1){
+            if (grade == -1) {
                 break;
             }
             grades.add(grade);
         }
 
-        if (grades.isEmpty()){
+        if (grades.isEmpty()) {
             System.out.println("No grades were entered.");
             return;
-        } 
+        }
+        sc.close();
 
         int sum = 0;
         int highest = grades.get(0);
         int lowest = grades.get(0);
 
-        for (int grade : grades){
-            if (grade > highest){
+        for (int grade : grades) {
+            if (grade > highest) {
                 highest = grade;
             }
-            if (grade < lowest){
+            if (grade < lowest) {
                 lowest = grade;
             }
             sum += grade;
